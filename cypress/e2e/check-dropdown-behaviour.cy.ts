94073e96cddb875075387bce44ca54e6f2da1f22
Sergei Baranovski <sergei@deriv.com>
2023-10-03 15:55:35 +0300

    feat: add selector alias

--- cypress/e2e/check-dropdown-behaviour.cy.ts
+++ cypress/e2e/check-dropdown-behaviour.cy.ts
@@ -1,21 +1,22 @@
 describe('Dropdown behaviour', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.get('input#rc_select_0').as('mainInput');
   });
 
   it('should have a dropdown', () => {
-    cy.get('input#rc_select_0');
+    cy.get('@mainInput');
   });
 
   it('should have an active dropdown after click', () => {
-    cy.get('input#rc_select_0').click();
+    cy.get('@mainInput').click();
     cy.contains('Forex').click();
   });
 
   it('should have a chart', () => {
     cy.get('div.recharts-wrapper').should('not.exist');
     cy.contains('Please select');
-    cy.get('input#rc_select_0').click();
+    cy.get('@mainInput').click();
     cy.contains('Forex').click();
     cy.get('[title="AUD/JPY"] > .ant-cascader-menu-item-content').click();
     cy.get('div.recharts-wrapper');
