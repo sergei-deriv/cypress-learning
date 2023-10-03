92b6a431744366c5f97d4bf308ae71d9d3925258
Sergei Baranovski <sergei@deriv.com>
2023-10-03 14:46:44 +0300

    feat: add baseUrl to config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5173/simple-dtrader-vite',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // on(
--- cypress/e2e/first-test.cy.ts
+++ cypress/e2e/first-test.cy.ts
@@ -1,10 +1,12 @@
 describe('First test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173/simple-dtrader-vite/');
+    // cy.visit('http://localhost:5173/simple-dtrader-vite/');
+    cy.visit('/');
   });
 
   it('should have a dropdown', () => {
-    cy.get('input#rc_select_0').should('exist');
+    cy.get('input#rc_select_0');
+    cy.get('input#rc_select_1').should('not.exist');
   });
 
   it('should have the words "Please select"', () => {
